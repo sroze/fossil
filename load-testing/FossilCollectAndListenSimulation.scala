a2a684d9f63a01f4cc50d711d9cdd5468179f9b2
Samuel ROZE <samuel.roze@gmail.com>
2019-11-05 12:12:41 +0000

    Add another load testing scenario and plug-in PgHero

--- load-testing/FossilCollectAndListenSimulation.scala
+++ load-testing/FossilCollectAndListenSimulation.scala
@@ -6,24 +6,19 @@ import scala.concurrent.duration._
 import scala.util.Properties
 import java.util.UUID.randomUUID
 
-class FossilSimulation extends Simulation {
+class FossilCollectAndListenSimulation extends Simulation {
 
   val httpProtocol = http.baseUrl(Config.FossilUrl)
 
   // Configuration
-  // private val rampUpTime: FiniteDuration = 30.seconds
   private val duration: FiniteDuration = 30.minutes
 
   setUp(
     Collector.collectMultipleEventsScenario.inject(rampUsers(Config.NumberOfStreams) during duration)
       .protocols(httpProtocol)
       .throttle(
-        reachRps(50) in (30 seconds),
-        holdFor(1 minute),
-        reachRps(100) in (5 minutes),
-        holdFor(10 minutes),
-        reachRps(200) in (5 minutes),
-        holdFor(10 minutes)
+        reachRps(100) in (1 minute),
+        holdFor(FiniteDuration),
       ),
     Consumer.listenEventsFor2Minutes.inject(
       rampUsers(50) during duration
